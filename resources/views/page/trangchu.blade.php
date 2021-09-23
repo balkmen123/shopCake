94a21f5dde2432e2468683c3f4e3afe214cfb2bc
Cao Hoai Nam <balkmen123@gmail.com.vnm>
2021-09-23 10:15:00 +0700

    f1

--- resources/views/page/trangchu.blade.php
+++ resources/views/page/trangchu.blade.php
@@ -99,7 +99,7 @@
                                     </div>
                                     <div class="single-item-caption">
                                         <a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
-                                        <a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+                                        <a class="beta-btn primary" href="{{route('chitiet',$spkm->id)}}">Details <i class="fa fa-chevron-right"></i></a>
                                         <div class="clearfix"></div>
                                     </div>
                                 </div>
